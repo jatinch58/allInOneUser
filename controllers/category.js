30f9f68f852eac13262033501fd4b724dbd36738
unknown <jatinch58@gmail.com>
2022-10-13 16:06:14 +0530

    get all categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,17 +1,69 @@
 const Category = require("../models/category");
-exports.getCategory = async (req, res) => {
+exports.getAllCategory = async (req, res) => {
   try {
-    const categories = await Category.find({}).populate([
-      "subCategory",
-      "service",
-    ]);
-    if (categories) {
-      return res.status(200).json({ result: categories });
+    let category = await Category.find(
+      {},
+      { _id: 1, name: 1, subCategory: 1, createdAt: 1, updatedAt: 1 }
+    )
+      .populate({
+        path: "subCategory",
+        select: { _id: 1, name: 1, createdAt: 1, updatedAt: 1 },
+        populate: {
+          path: "subCategory2",
+          select: { _id: 1, name: 1, service: 1, createdAt: 1, updatedAt: 1 },
+          populate: {
+            path: "service",
+            model: "service",
+            select: {
+              _id: 1,
+              name: 1,
+              description: 1,
+              rating: 1,
+              image: 1,
+              createdAt: 1,
+              updatedAt: 1,
+            },
+          },
+        },
+      })
+      .populate({
+        path: "subCategory",
+        model: "subCategory",
+        select: { _id: 1, name: 1, createdAt: 1, updatedAt: 1 },
+        populate: {
+          path: "service",
+          model: "service",
+          select: {
+            _id: 1,
+            name: 1,
+            description: 1,
+            rating: 1,
+            image: 1,
+            createdAt: 1,
+            updatedAt: 1,
+          },
+        },
+      })
+      .populate("service", {
+        name: 1,
+        description: 1,
+        rating: 1,
+        image: 1,
+        createdAt: 1,
+        updatedAt: 1,
+      });
+
+    if (!category) {
+      return res
+        .status(500)
+        .send({ success: false, message: "Something went wrong" });
     }
-    return res.status(500).json({ message: "Something went wrong" });
+    return res.status(200).send({
+      success: true,
+      message: "All Category SubCategory SubCategory2 fetched successfully",
+      category,
+    });
   } catch (e) {
-    return res
-      .status(500)
-      .json({ message: "Something went wrong", error: e.name });
+    return res.status(500).send({ success: false, error: e.name });
   }
 };
--- routes/category.js
+++ routes/category.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const category = require("../controllers/category");
 const { verifyToken } = require("../middlewares/auth");
-router.get("/allCategory", category.getCategory);
+router.get("/allCategory", category.getAllCategory);
 module.exports = router;
